2312b33aa76a7b3d5ddd10c777c8203f58d2a018
NadiaSchmitz <nadiiaschmitz@gmail.com>
2021-12-22 12:16:58 +0100

    First commit

---
+++ src/EinmaleinsTabelle.java
@@ -0,0 +1,24 @@
+
+public class EinmaleinsTabelle {
+
+	public static void main(String[] args) {
+		
+		int i, j;
+		
+		for (i = 1; i < 10; i++) {
+			
+			System.out.println("----------");
+			System.out.println("    " + i);
+			System.out.println("----------");
+			
+			for (j = 1; j < 10; j++) {
+				
+				System.out.println(i + " * " + j + " = " + i * j);
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
